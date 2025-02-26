08b8aced05d74a5d2482315a9bf341db93fc4b34
Lakshan03X <nipunlakshan9255@gmail.com>
2025-02-26 20:29:06 +0530

    error page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,17 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter ,RouterProvider } from 'react-router-dom'
+
+import Error from './components/Error/err_404_page.jsx'
 import Home from './components/HomePage/Home.jsx'
 import Contact from './pages/Contact.jsx'
 import About from './pages/About.jsx'
 import Store from './pages/Store.jsx'
 import Login from './components/LoginPage/Login.jsx'
 
+
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <h1 style={{ color: "black" }}>404 Not Found</h1>,
+    errorElement: <Error/>,
+    // errorElement: <h1 style={{ color: "black" }}>404 Not Found</h1>,
     children: [
       {
         index: true,
