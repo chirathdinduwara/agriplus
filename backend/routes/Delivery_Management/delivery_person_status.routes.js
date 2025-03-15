a7b9368c9685c2896f1046a0163cee3836599fbf
Lakshan03X <nipunlakshan9255@gmail.com>
2025-03-15 14:49:05 +0530

    done

--- backend/routes/Delivery_Management/delivery_person_status.routes.js
+++
@@ -1,8 +0,0 @@
-import { updateDeliveryStatus } from "../../controllers/Delivery_Management/delivery_person_status.controller.js";
-import express from "express";
-
-const router = express.Router();
-
-router.put("/update_status", updateDeliveryStatus);
-
-export default router;
