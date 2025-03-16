b1d0ffbfab2f0dfcd14f46ed2bf5731ec48ab3a3
Lakshan03X <nipunlakshan9255@gmail.com>
2025-03-16 18:20:06 +0530

    bug fix

---
+++ backend/models/delivery_model/del_person_status.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const statusSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true 
+    },
+    full_name: {
+        type: String,
+        required: true
+    },
+    shipping_address: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: Number,
+        required: true
+    }
+}, { timestamps: true }); 
+
+const Delivery_Status = mongoose.model('Delivery_Status', statusSchema);
+export default Delivery_Status;
