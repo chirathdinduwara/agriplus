9fd915aba2a46de8409b65c6997faf6d5ed414ae
Lakshan03X <nipunlakshan9255@gmail.com>
2025-05-03 19:37:26 +0530

    discover button and animation added

--- frontend/src/components/HomePage/Home.jsx
+++ frontend/src/components/HomePage/Home.jsx
@@ -1,21 +1,28 @@
 import "../../css/HomePage/home.css";
 import videoBg from "../../assets/videos/home-bg.mp4";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-    return(
-        <>
-            <div className="home">
-                <div className="overlay"></div>
-                <video className='home-bg' src={videoBg} autoPlay loop muted />
-                <div className="home-hero">
-                    <p className='home-hero-heading'>Sri Lanka</p>
-                    <p className="home-hero-heading2">BEST HARVEST</p>            
-                    <p className="home-hero-heading3">IN THE WORLD</p>   
-                    <button className='log-in gap'>Discover More</button>  
-                </div>
-            </div>
-        </>
-    );
+  const navigate = useNavigate();
+  const goShop = () => {
+    navigate("/store");
+  };
+  return (
+    <>
+      <div className="home">
+        <div className="overlay"></div>
+        <video className="home-bg" src={videoBg} autoPlay loop muted />
+        <div className="home-hero">
+          <p className="home-hero-heading">Sri Lanka</p>
+          <p className="home-hero-heading2">BEST HARVEST</p>
+          <p className="home-hero-heading3">IN THE WORLD</p>
+          <button className="log-in gap" onClick={goShop}>
+            Discover More
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
