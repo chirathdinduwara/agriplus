b043155c9dd4dbc8deb089163665e03a14dbe952
chirathdinduwara <tdchirathinduwara@gmail.com>
2025-05-11 23:38:28 +0530

    fe bg fixed

--- frontend/src/components/AdminDashboard/AdminDash.jsx
+++ frontend/src/components/AdminDashboard/AdminDash.jsx
@@ -1,5 +1,33 @@
+import '../../css/AdminDashboard/adminDashboard.css';
+import InfoCard from '../../components/StaffDashboard/InfoCard';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 function AdminDash() {
-    return ("ADDED LATER ...");
+    const [categoryCounts, setCategoryCounts] = useState({
+        Admins: 0,
+        Farmers: 0,
+        Staff: 0
+    });
+
+    useEffect(() => {
+
+                const counts = {
+                    Admins: 5,
+                    Farmers: 101,
+                    Staff: 20
+                };
+
+                setCategoryCounts(counts);
+    }, []);
+
+    return (
+        <div className="staff-dashboard">
+            <InfoCard title="Admins" count={categoryCounts.Admins} />
+            <InfoCard title="Farmers" count={categoryCounts.Farmers} />
+            <InfoCard title="Staff" count={categoryCounts.Staff} />
+        </div>
+    );
 }
 
 export default AdminDash;
